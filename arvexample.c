60bbef8d57f1580023d50947d3f6db98b5e35acc
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-01-31 02:12:30 +0100

    i see a camera image now, colors look a bit weird and occasionally there is a black line

--- arvexample.c
+++ arvexample.c
@@ -1,12 +1,103 @@
 //  gcc -g -O2 -o arv-example arvexample.c -MD -MP -MF -pthread -I/usr/include/aravis-0.4 -I/usr/lib/glib-2.0/include  -I/usr/include/glib-2.0  -lm -L/usr/lib -lgio-2.0 -lgobject-2.0 -lxml2 -lgthread-2.0 -pthread -lrt -lglib-2.0 -lz  -laravis-0.4 -lglfw
 #include <GLFW/glfw3.h>
-
+#include <string.h>
 #include <arv.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <stdio.h>
 
-GLFWwindow* window;
+enum { W = 658, H = 494};
+
+unsigned short store[W*H];
+
+pthread_mutex_t mutex_texture = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  condition_new_image   = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t mutex_dontquit = PTHREAD_MUTEX_INITIALIZER;
+int dontquit=1;
+
+void write_dontquit(int b)
+{
+  pthread_mutex_lock( &mutex_texture );
+  dontquit=b;
+  pthread_mutex_unlock( &mutex_texture );
+}
+
+
+int read_dontquit()
+{
+  pthread_mutex_lock( &mutex_texture );
+  int b=dontquit;
+  pthread_mutex_unlock( &mutex_texture );
+  return b;
+}
+
+void* gl(void*ignore)
+{
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+  GLFWwindow* window = glfwCreateWindow(W, H, "gig-e-camera", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  GLuint texture;
+  glGenTextures( 1, &texture );
+  glBindTexture( GL_TEXTURE_2D, texture );
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                     GL_NEAREST );
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+  //glMatrixMode(GL_COLOR);
+  //glPushMatrix();
+  //glLoadIdentity();
+  //double s=10000*65536/2048.0;
+  //double o=0;
+  //glScaled(s,s,s);
+  //glTranslated(-o,-o,-o);
+  glTexImage2D(GL_TEXTURE_2D,0,GL_LUMINANCE16,W,H,0,GL_LUMINANCE,GL_UNSIGNED_SHORT,0);
+  //glPopMatrix();
+  //glMatrixMode(GL_MODELVIEW);
+  glEnable(GL_TEXTURE_2D);
+
+  while (read_dontquit()) {
+    //pthread_mutex_lock( &mutex_texture );
+    // Wait while reader function new_buffer_cb copies data
+    // mutex unlocked if condition variable in new_buffer_cb  signaled.
+    //pthread_cond_wait( &condition_new_image, &mutex_texture );
+
+    glTexSubImage2D(GL_TEXTURE_2D,0,0,0,W,H,GL_LUMINANCE,GL_UNSIGNED_SHORT,store);
+ 
+
+    FILE*f=fopen("/dev/shm/bla.pgm","w");
+    fprintf(f,"P5\n658 494\n65535\n");
+    fwrite(store,sizeof(store),1,f);
+    fclose(f);
+
+
+    //pthread_mutex_unlock( &mutex_texture );
+    int width,height;
+    glfwGetFramebufferSize(window, &width, &height);
+    float ratio = width / (float) height;
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glOrtho(0, W, 0, H, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glColor3f(1.f,1.f,1.f);
+    glBegin(GL_QUADS);
+    glVertex2i(0,0); glTexCoord2i(0,0);
+    glVertex2i(0,H); glTexCoord2i(1,0);
+    glVertex2i(W,H); glTexCoord2i(1,1);
+    glVertex2i(W,0); glTexCoord2i(0,1);
+    glEnd();
+     
+    glfwSwapBuffers(window);
+    usleep(32000);
+  }
+
+  glfwTerminate();
+}
 
 typedef struct {
 	GMainLoop *main_loop;
@@ -21,6 +112,9 @@ set_cancel (int signal)
 	cancel = TRUE;
 }
 int count=0;
+
+#define min(a,b) (((a)<(b))?(a):(b))
+
 static void
 new_buffer_cb (ArvStream *stream, ApplicationData *data)
 {
@@ -34,34 +128,21 @@ new_buffer_cb (ArvStream *stream, ApplicationData *data)
 		char s[100];
 		snprintf(s,sizeof(s),"/dev/shm/dat/o%04d.pgm",count);
 		FILE*f=fopen(s,"w");
-		fprintf(f,"P5\n659 494\n65535\n");
+		fprintf(f,"P5\n658 494\n65535\n");
 		count++;
 		fwrite(buffer->data,buffer->size,1,f);
 		fclose(f);
 
-		int width,height;
-glfwGetFramebufferSize(window, &width, &height);
-float ratio = width / (float) height;
-glViewport(0, 0, width, height);
- glClear(GL_COLOR_BUFFER_BIT);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-glMatrixMode(GL_MODELVIEW);
-glLoadIdentity();
-glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-glBegin(GL_TRIANGLES);
-glColor3f(1.f, 0.f, 0.f);
-glVertex3f(-0.6f, -0.4f, 0.f);
-glColor3f(0.f, 1.f, 0.f);
-glVertex3f(0.6f, -0.4f, 0.f);
-glColor3f(0.f, 0.f, 1.f);
-glVertex3f(0.f, 0.6f, 0.f);
-glEnd();
-
-		glfwSwapBuffers(window);
+		//pthread_mutex_lock(&mutex_texture);
+		int i;
+		for(i=0;i<W*H;i++)
+		store[i]=(1<<16)/(1<<12)*(buffer->data[2*i]+256*buffer->data[2*i+1]);
+		//memcpy(store,buffer->data,min(sizeof(store),buffer->size));
+		/*pthread_cond_signal( &condition_new_image );
+		pthread_mutex_unlock(&mutex_texture);
+		*/
 		if (count==1000){
-		  g_main_loop_quit (data->main_loop);
+		  //g_main_loop_quit (data->main_loop);
 		}
 		arv_stream_push_buffer (stream, buffer);
 	}
@@ -92,7 +173,7 @@ control_lost_cb (ArvGvDevice *gv_device)
 	cancel = TRUE;
 }
 
-enum { W = 659, H = 494};
+
 
 int
 main (int argc, char **argv)
@@ -103,10 +184,8 @@ main (int argc, char **argv)
 	ArvBuffer *buffer;
 	int i;
 
-	if (!glfwInit())
-	  exit(EXIT_FAILURE);
-        window = glfwCreateWindow(W, H, "gig-e-camera", NULL, NULL);
-	glfwMakeContextCurrent(window);
+	pthread_t gl_thread;
+	int iret1 = pthread_create( &gl_thread, NULL, gl, (void*) NULL);
 
 	data.buffer_count = 0;
 
@@ -142,7 +221,7 @@ main (int argc, char **argv)
 		/* Set frame rate to 10 Hz */
 		arv_camera_set_frame_rate (camera, 100.0);
 		arv_camera_set_gain (camera, 100);
-		arv_camera_set_exposure_time (camera, 1000.0 /*us*/);
+		arv_camera_set_exposure_time (camera, 390.0 /*us*/);
 		/* retrieve image payload (number of bytes per image) */
 		payload = arv_camera_get_payload (camera);
 		
@@ -191,6 +270,7 @@ main (int argc, char **argv)
 	} else
 		printf ("No camera found\n");
 
-	glfwTerminate();
+	write_dontquit(0);
+	pthread_join( gl_thread, NULL);
 	return 0;
 }
