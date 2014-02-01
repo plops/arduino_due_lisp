70fc49e6f13b0c6c58585a91fa1670f7216f4f9b
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-02-01 12:39:59 +0100

    use qsort to give an example on how much type information is stored in the ffi database

--- arvexample-ccl.lisp
+++ arvexample-ccl.lisp
@@ -14,12 +14,69 @@ ccl::*eeps* ;; hash table with external functions
 ;; the following code opens a cdb file, a read-only data structure,
 ;; that ccl uses to store foreign function definitions. 
 (defparameter *fun*
-  (ccl::cdb-open "/home/martin/src/ccl/x86-headers64/arv/functions.cdb"))
+  (ccl::cdb-open "/home/martin/src/ccl/x86-headers64/libc/functions.cdb"))
 ;; using cdb-enumerate-keys, all function names can be obtained (as
 ;; strings) and db-lookup-function gives access to function arguments
 ;; and return type
 (ccl::cdb-enumerate-keys *fun*)
-(ccl::db-lookup-function *fun* "arv_camera_new")
+;; this is a part of the strings that are returned:
+;; ... "nis_ping_3_svc" "__roundl" "__y1f" "fchownat"
+;; "__rpc_thread_createerr" "fexecve" "getpwent" "htons"
+;; "nfsproc_rename_2_svc" "wscanf" "atan2f" "__powf" "wcsnlen"
+;; "__lround" "__pow10f" "la_preinit" "getmsg" "alloca"
+;; "td_thr_getgregs" "sigqueue" "crypt" "getpgid" "truncate64"
+;; "daemon" "pipe" "yp_unbind" "ypproc_domain_nonack_2" ...
+
+(ccl::db-lookup-function *fun* "qsort")
+;; => #S(CCL::EXTERNAL-FUNCTION-DEFINITION :ENTRY-NAME "qsort"
+;; :ARG-SPECS (:ADDRESS :SIZE_T :SIZE_T :__COMPAR_FN_T) :RESULT-SPEC
+;; :VOID :MIN-ARGS 4)
+
+;; compare this to the definition from the /usr/include/stdlib.h:
+
+;; /* Sort NMEMB elements of BASE, of SIZE bytes each,
+;;    using COMPAR to perform the comparisons.  */
+;; extern void qsort (void *__base, size_t __nmemb, size_t __size,
+;;                    __compar_fn_t __compar) __nonnull ((1, 4));
+
+;; and the ffi definition ccl/x86-headers64/libc/C/usr/include/stdlib.ffi:
+;; (function ("/usr/include/stdlib.h" 762)
+;;  "qsort"
+;;  (function
+;;   ((pointer (void ())) (typedef "size_t") (typedef "size_t") (typedef "__compar_fn_t") )
+;;   (void ())) (extern))
+
+;; apparently the attribute __nonnull((1,4)) got lost. i would guess
+;; this indicates that the start of the array (__base) and the
+;; comparison function (__compar) should not be null
+
+;; the function type definition of __compar_fn_t loses the __const:
+
+;; typedef int (*__compar_fn_t) (__const void *, __const void *);
+
+;; (type ("/usr/include/stdlib.h" 742)
+;;  "__compar_fn_t"
+;;  (pointer (function
+;;   ((pointer (void ())) (pointer (void ())) )
+;;   (int ()))))
+
+
+
+
+
+(ccl::db-lookup-function *fun* "vsnprintf")
+;; => #S(CCL::EXTERNAL-FUNCTION-DEFINITION :ENTRY-NAME "vsnprintf"
+;; :ARG-SPECS (:ADDRESS :SIZE_T :ADDRESS :ADDRESS) :RESULT-SPEC
+;; :SIGNED-FULLWORD :MIN-ARGS 4)
+
+;; let's have a look at a function with a variable number of
+;; arguments:
+(ccl::db-lookup-function *fun* "snprintf")
+;; => #S(CCL::EXTERNAL-FUNCTION-DEFINITION :ENTRY-NAME "snprintf"
+;; :ARG-SPECS (:ADDRESS :SIZE_T :ADDRESS :VOID) :RESULT-SPEC
+;; :SIGNED-FULLWORD :MIN-ARGS 4)
+
+
 ;; ftd .. foreign type data, this contains a list of the directories
 ;; to search, can be expanded with use-interface-dir (which in turn
 ;; calls move-dll-nodes from compiler/dll-node.lisp)
