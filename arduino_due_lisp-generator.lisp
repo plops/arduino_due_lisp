2d5efa2587c3ab286456a684cfa452846fb03b6b
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-02-09 09:47:32 +0100

    prepare a generator for the ino file that should make it easier to only include the code that is necessary to play with one particular hardware configuration

---
+++ arduino_due_lisp-generator.lisp
@@ -0,0 +1,85 @@
++HEADERS+
+#include <SPI/SPI.h>
+
++FUNCTIONS+
+
+const int chipSelectPin = 16;
+void setup_max532()
+{
+  // start the SPI library:
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE2);
+ //SPI.setClockDivider(); //default clock of 4MHz should work
+  
+  // initalize the  chip select pins:
+  pinMode(chipSelectPin, OUTPUT);
+  analogReadResolution(12);
+}
+
+void writeDAC(unsigned short b, unsigned short a)
+{
+  digitalWrite(chipSelectPin, LOW);
+  byte x,y,z;
+  x = (0xff0 & a) >> 4;
+  y = ((0xf & a) << 4) + ((0xf00 & b) >> 8);
+  z = 0xff & b;
+  SPI.transfer(x); 
+  SPI.transfer(y); 
+  SPI.transfer(z); 
+  digitalWrite(chipSelectPin, HIGH);
+}
+
+
++FUNCTIONS_ENUM+
+F_DAC, F_DIGITALWRITE, F_PINMODE, F_ADC, F_DELAY, F_DELAYMICROSECONDS, F_MICROS, F_ROOM
+
++FUNCTIONS_NAMES+
+"dac", "digital-write", "pin-mode",
+"adc", "delay", "delay-microseconds", "micros", "room"
+
+	+FUNCTIONS_STACK_PROCESSING+
+	case F_DAC:
+	  argcount("dac", nargs, 2);
+	  writeDAC(tonumber(Stack[SP-2],"dac"),tonumber(Stack[SP-1],"dac"));
+	    v=T;
+	  break;
+	case F_DIGITALWRITE:
+	  argcount("digital-write", nargs, 2);
+	  digitalWrite(tonumber(Stack[SP-2],"digital-write"),tonumber(Stack[SP-1],"digital-write"));
+	    v=T;
+	  break;
+	case F_PINMODE:
+	  argcount("pin-mode", nargs, 2);
+	  pinMode(tonumber(Stack[SP-2],"pin-mode"),tonumber(Stack[SP-1],"pin-mode"));
+	  v=T;
+	  break;
+	case F_ADC:
+	  argcount("adc", nargs, 1);
+	  v = number(analogRead(tonumber(Stack[SP-1],"adc")));
+	  break;
+	case F_DELAY:
+	  argcount("delay", nargs, 1);
+	  delay(tonumber(Stack[SP-1],"delay"));
+	  v = T;
+	  break;
+	case F_DELAYMICROSECONDS:
+	  argcount("delay-microseconds", nargs, 1);
+	  delayMicroseconds(tonumber(Stack[SP-1],"delay-microseconds"));
+	  v = T;
+	  break;
+	case F_MICROS:
+	  argcount("micros", nargs, 0);
+	  v = number(micros());
+	  break;
+	case F_ROOM:
+	  argcount("room", nargs, 0);
+	  {
+	    char s[80];
+	    snprintf(s,sizeof(s), "heap: %d/%d, stack: %d/%d",
+		     (curheap-fromspace)/8, heapsize/8, SP, N_STACK);
+	    Serial.println(s);
+	  }
+	  v = number((curheap-fromspace)/8);
+	  break;
+
