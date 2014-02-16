6cb07330f28d3391357aa8335ed2b735af567a6e
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-02-16 20:56:05 +0100

    something is wrong with matlab or dipimage. it always crashes with my datasets

--- process.m
+++ process.m
@@ -1,21 +1,25 @@
 %% convert pgm 16-bit data to tif with imagej
 % the following was acquired with the sony icx ccd sensor
-tic
-a = DampEdge(readtimeseries('/dev/shm/20140215/20140215_2.tif'),.08,2);
+a = readtimeseries('/dev/shm/20140215/20140215_2.tif');
+a=a.*gaussf(rr([512 494],'freq')<.45,8);
+ka = newim(a,'complex');
+for k=0:size(a,3)-1
+	ka(:,:,k) = ft(a(:,:,k))
+end
+	
 ka = dip_fouriertransform(a,'forward',[1 1 0]);
-toc % 145 s on one core
+clear a
 
 % cut out the first order
 ka = extract(ka,[82 82],[220 63]);
 
 % inverse fourier transform
-a2 =   dip_fouriertransform(ka,'inverse',[1 1 0]);
+a2 = dip_fouriertransform(ka,'inverse',[1 1 0]);
 a2 = reshape(a1,[82 82 23 23]);
 save '/home/martin/dat/20140215_2' a2
 
-% filesize is 26Mb
-
 %%
+% now open the cmos data
 a = readtimeseries('/dev/shm/20140215/1/20140215_1.tif');
 % DampEdge doesnt run on my data, and is not necessary
 ka = dip_fouriertransform(a,'forward',[1 1 0]);
