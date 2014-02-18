7267b930fe71ffe05dc8e9fa9987ed6f663c05c6
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-02-18 01:36:37 +0100

    i have a hard time extracting the first order of the cmos images because the data is aliased

--- process2.m
+++ process2.m
@@ -15,8 +15,36 @@
 % color number), but loading my pgm images seems to work anyway
 
 folder = '/dev/shm/20140217/'
-fns=dir([folder '*_2_*.pgm']); % get all filenames of images of camera 2
-temp=readim([folder fns(1).name]); % just open one image to get the dimensions
+fns=dir([folder '*_2_*.pgm']); % get all filenames of images of camera
+2
+temp=readim([folder fns(1).name]); % just open one image to get the
+dimensions
+w = size(temp,1);
+h = size(temp,2);
+z = size(fns,1);
+im = newim([w h z], 'single');
+clear temp;
+for k=1:size(fns)
+  A=sscanf(fns(k).name,'i%d_j%d_%d_%g.pgm');
+  exposure=A(4);
+% the 100 is an artificial offset that i add during acquisition when
+% subtracting the background
+  im(:,:,k-1)=(readim([folder fns(k).name])-100)./exposure;
+end
+
+%im=reshape(im,[w h 16 16]);
+%im=reshape(im,[w h 256]);
+
+a2 = newim([96 96 z],'complex');
+for k=0:z-1
+  a2(:,:,k) = ift(extract(ft(squeeze(im(:,:,k))),[96 96],[156 69]));
+end
+
+writeim(abs(a2),'/dev/shm/a2.tif')
+
+
+fns=dir([folder '*_1_*.pgm']); % get all filenames of images of camera 1
+temp=readim([folder fns(1).name]); 
 w = size(temp,1);
 h = size(temp,2);
 z = size(fns,1);
@@ -28,14 +56,21 @@ for k=1:size(fns)
   im(:,:,k-1)=(readim([folder fns(k).name])-100)./exposure;
 end
 
-%im=reshape(im,[w h 16 16]);
-%im=reshape(im,[w h 256]);
 
-log(abs(ft(im(:,:,32))))
+  % due to the large pixel size of the cmos i ended up with an aliased
+% hologram, in order to easy extracting the first order i use fftn
+% without fftshift
+%ka=newim([w h],'single');
+%for k=0:z-1
+%  ka(:,:)=ka(:,:)+abs(dip_image(fftn(dip_array(im(:,:,k)))));
+%end
+%extract(ka,[112 112],[259 57])
 
-ka=dip_fouriertransform(im,'forward',[1 1 0])
 
-mean(ans,[],3)
-proj=ans;
+a1 = newim([112 112 z],'complex');
+for k=0:z-1
+  a1(:,:,k) = ift(dip_image(fftshift(dip_array(extract(dip_image(fftn(dip_array(squeeze(im(:,:,k))))),[112 112],[259 57])))));
+end
 
-extract(proj,[96 96],[156 69]);
+writeim(abs(a2),'/dev/shm/a2.tif')
+
