4e231e1cd26363117a9855a3020e2c199d5611ee
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-02-18 03:41:56 +0100

    copy volkov_unwrap.m into new file

--- process2.m
+++ process2.m
@@ -85,26 +85,5 @@ for k=0:z-1
   a1b(:,:,k)=ift(extract(ft(a1(:,:,k)),[256 256]));
 end
 
-% volkov 2003 phase unwrapping in the presence of noise
-clear i;
-z=squeeze(a1b(:,:,70));
-zx=real((dx(real(z))+i*dx(imag(z)))./(i*z));
-zy=real((dy(real(z))+i*dy(imag(z)))./(i*z));
-
-%% this is equation 3 from the paper
-ka=(ft(zx).*xx(zx,'freq') + ft(zy).*yy(zy,'freq'))./rr(zx,'freq').^2;
-ka(floor(size(ka,1)/2),floor(size(ka,2)/2))=0;
-% first estimation of unwrapped phase (according to paper around 2% error)
-ph1=real(ift(ka)/(2*pi*i))
-
-
-kx=(zx-dx(ph1))/(2*pi);
-ky=(zy-dy(ph1))/(2*pi);
-ka=(ft(kx).*xx(kx,'freq') + ft(ky).*yy(ky,'freq'))./rr(kx,'freq').^2;
-ka(floor(size(ka,1)/2),floor(size(ka,2)/2))=0;
-% reconstructed integer field
-k_recon=round(real(ift(ka)/(2*pi*i)));
-
-% using the integer field, produce a better correction of the unwrapped phase
-ph2=ph1-2*pi*k_recon;
-%cat(3,ph1,ph2,(phase(exp(i*ph2))-phase(z))/(2*pi),ph2-(phase(exp(i*ph2))-phase(z)))
+volkov_unwrap(squeeze(a1b(:,:,70)));
+addpath('/home/martin/Arduino/arduino_due_lisp/')
