a9e99b13005027da36196e1f1075c66588c4553d
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-04-15 09:28:37 +0200

    add comments to the julia code so that i can use it as a tutorial

--- processing.jl
+++ processing.jl
@@ -1,37 +1,63 @@
 x = 1.0
 
-for T = {Int8,Int16,Int32,Int64,Int128,Uint8,Uint16,Uint32,Uint64,Uint128}
-    println("$(lpad(T,7)): [$(typemin(T)),$(typemax(T))]")
-end
+
+typeof(x)
 
 eps(Float64)
 
+
+# complex numbers
 z=1+2im
+
+typeof(z)
+
+z=1.0+2im
+
+typeof(z)
+
 real(z)
 imag(z)
 conj(z)
 abs(z)
 abs2(z)
 angle(z)
-complex(1,3)
+complex(1,3) # explicit construction 
 
-
+# plain function:
 function f(x,y)
     x + y
 end
 
+f(1,2)
 
+# anonymous functions:
+x->x^2
+
+(x->x^2)(2)
+
+# apply function to elements in an array:
 map(x->x^2+2x-1,[1,3,-1])
+
 map(x->x^2+2x-1,[1 3 -1])
 
+# anonymous function with two arguments
+(x,y)->(x+y)
 
-## function f(x,y)
-##     x + y, x*y
-## end
+((x,y)->(x+y))(3,4)
 
+
+# function returning multiple values:
+function f2(x,y)
+    x + y, x*y
+end
+
+f2(2,3)
+
+# tuple
 x= (3,4)
-f(x...)
 
+
+# optional function arguments:
 function blop(num, base=10)
     base + num
 end
@@ -39,12 +65,14 @@ blop(10)
 blop(10,1)
 
 
+# keyword arguments:
 function psf(x,y,z; na=1.4, ri=1.518)
     [x y z na ri]
 end
 psf(12, 12, 12, na=1.)
 
 
+# another way to define an anonymous function
 map([1, 2, 4]) do x
     if x<0 && iseven(x)
         0
@@ -55,6 +83,8 @@ map([1, 2, 4]) do x
     end
 end
 
+# this "do x" syntax is useful when handling files: this opens a the
+# file "/dev/shm/outfile" and writes the string "bla" into it
 cd("/dev/shm/") do
     open("outfile","w") do f
         write(f,"bla")
@@ -62,6 +92,7 @@ cd("/dev/shm/") do
 end
 
 
+# two ways of defining code blocks:
 z = begin
     x=1
     y=3
@@ -71,6 +102,8 @@ end
 z = (x=1;y=4;x+y)
 
 # conditional expressions must not be anything but true or false
+
+# several ways to write loop constructs
 
 i=0;
 while i<4
@@ -86,38 +119,100 @@ for s in [1 2 3 4]
     println(s)
 end
 
+
+# this will iterate over both variables (outer product)
 for i=0:2, j=3:4
     println([i j])
 end
 
 whos()
 
-copy(a)
+
+# different ways to copy an array:
+a=[1 2 3]
+b=a
+
+b[1]=12
+
+a
+
+
+# shallow copy will use new memory:
+a=[1 2 3]
+b=copy(a)
+
+b[1]=12
+
+a
+
+# there is also this: 
 deepcopy(a)
 
-(x = rand(8);Float64[ .25x[i-1]+.5x[i]+.25x[i+1] for i=2:length(x)-1])
+#(x = rand(8);Float64[ .25x[i-1]+.5x[i]+.25x[i+1] for i=2:length(x)-1])
 
-SVD(x,true) # for thinning
+#SVD(x,true) # for thinning
 
-diag
+#diag
 
 # julia is 1 based
 # arrays are column major ordered (fortran)
 
 extrema([1 2 3 4 5])
+
 findmin([1 2 3 4 5])
 
+
+# string concatenation    
 "Hallo" * " Welt"
 
 
+ccall((:sin,"libm"),Float64,(Float64,),12.0)
 
 
+# matlab uses saturated integer arithmetic:
+# adding to a big value, value stays the same
+# not efficient
+# not associative
+# makes it hard to write many integer algorithms
+# prevents aggressive optimization
+
+# julia makes some better assumptions, the authors argue and give this
+# neat example:
+
+
+f(k) = 5k-1
+
+
+help(code_native)
+
+code_native(f,(Int,))
+
+function g(k)
+    for i=1:10
+        k=f(k)
+    end
+    return k
+end
+
+code_native(g,(Int,))
+
+# compiler (llvm) optimizes the loop to a multiplication
+
+
+
+
+
+# compare string with a pattern:
 match(r"^j....\.ics$","j1234.ics").match
+
 match(r"^j....\.ics$","bla.mat").match
 
 
+# array functions similar to matlab:
 vcat([1,2,3],[4,5,6])
 
+
+# find all ics files in a folder
 dir = "/media/sda4/b/20140309/0_/"
 function find_ics_files(dir) 
     fns = readdir(dir)
@@ -133,10 +228,12 @@ end
 fns=find_ics_files(dir)
 
 
+# i use this to search for the end of the ics header in the files
 ismatch(r"^end$","end\n")
 
 
-
+# this function opens a file searches from the beginning for the
+# string "end" and returns the position
 function find_ics_raw_start(fn)
     # locate the word "end" in the ics file
     f=open(fn)
@@ -146,8 +243,13 @@ function find_ics_raw_start(fn)
     close(f)
     res
 end
-#find_ics_raw_start(dir * first(fns))
 
+find_ics_raw_start(dir * first(fns))
+
+# this function 
+# jumps over the header
+# and reads the raw data from the file into an array
+# (i keep the dimensions fixed for this example)
 function read_ics(fn)
     pos = find_ics_raw_start(fn)
     f=open(fn)
@@ -158,11 +260,22 @@ function read_ics(fn)
 end
 
 a = read_ics(dir * first(fns));
-          
+
+
+# check that the dimensions make sense
 (filesize(dir * first(fns))-519)/(80*84*16)
 
 size(a[:,:,1])
 
+
+
+# there is a nice interface for the browser in julia, but for now i
+# just store images as pgm files.
+
+
+# i make a 1d array of the input array then i find the maximum and
+# minimum values and create an array with 8bit version of the data,
+# scaled between 0 and 255
 function write_pgm(a,fn="/dev/shm/o.pgm")
     ar=reshape(a,prod(size(a)))
     mi,ma = extrema(ar)
@@ -171,6 +284,7 @@ function write_pgm(a,fn="/dev/shm/o.pgm")
         s=255/(ma-mi);
     else
         s=1;
+    end
     a8=uint8(min(255,max(0,floor((ar.-mi)*s))))
     f=open(fn,"w")
     @printf(f,"P5\n%d %d\n255\n",size(a,1),size(a,2));
@@ -180,15 +294,18 @@ end
 
 write_pgm(abs(a[:,:,1]))
 
+# try reading all the data (considerably faster than matlab)
 a=Array(Complex64,80,84,151,161);
 fns=find_ics_files(dir)
 i = 1;
-@time for f in fns
-    println(f)
+@time for file in fns
+    println(file)
     a[:,:,:,i] = read_ics(dir * fns[i])
     i = i+1;
 end
 
+
+# this is data from a second experiment:
 a2=Array(Complex64,80,84,151,161);
 dir2 = "/media/sda4/b/20140309/1_/"
 fns=find_ics_files(dir2)
@@ -219,10 +336,10 @@ aangb = (aang .> quantile(reshape(aang,151*161),.6));
 
 write_pgm(aang,"/dev/shm/aang.pgm")
 write_pgm(aangb,"/dev/shm/aangb.pgm")
-    asmall=reshape(a,80*84,151*161)[reshape(acamb,80*84),reshape(aangb,151*161)]
+asmall=reshape(a,80*84,151*161)[reshape(acamb,80*84),reshape(aangb,151*161)]
 
 
- asmall2=reshape(a2,80*84,151*161)[reshape(acamb,80*84),reshape(aangb,151*161)]
+asmall2=reshape(a2,80*84,151*161)[reshape(acamb,80*84),reshape(aangb,151*161)]
 
     ## julia> size(asmall)
 ## (3360,9724)
@@ -307,3 +424,14 @@ end
 
 # i only have openh264 right now and this encoder needs images sizes to be multiples of 16:
 # for i in r*.pgm ;do pnmcut -width 144 -height 160 $i > cut.pgm ; convert cut.pgm o.yuv ; cat o.yuv >> ../o.yuv;done
+
+
+
+
+
+
+
+for T = {Int8,Int16,Int32,Int64,Int128,Uint8,Uint16,Uint32,Uint64,Uint128}
+    println("$(lpad(T,7)): [$(typemin(T)),$(typemax(T))]")
+end
+
