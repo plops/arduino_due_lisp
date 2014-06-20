03082c99e29904469bcffa27b60712dd6881dc2d
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-06-20 12:02:08 +0200

    add more code to pylon wrapper

--- pylon/pylon_wrapper.cpp
+++ pylon/pylon_wrapper.cpp
@@ -28,6 +28,22 @@ extern "C" {
   void pylon_wrapper_create()
   {
     try{
+      CTlFactory& tlFactory = CTlFactory::GetInstance();
+      
+      // Get all attached devices
+      DeviceInfoList_t devices;
+      if ( tlFactory.EnumerateDevices(devices) == 0 )
+	printf("%s finds no cameras: %d\n",__func__,devices.size()); 
+
+      CInstantCameraArray cameras( min( devices.size(), c_maxCamerasToUse));
+
+      // Create and attach all Pylon Devices.
+      for ( size_t i = 0; i < cameras.GetSize(); ++i) {
+	cameras[ i ].Attach( tlFactory.CreateDevice( devices[ i ]));
+	
+	// Print the model name of the camera.
+	cout << "Using device " << cameras[ i ].GetDeviceInfo().GetModelName() << endl;
+      }
     }
     catch (GenICam::GenericException& e) {
       printf( "Exception caught in %s msg=%hs",__func__, e.what());
