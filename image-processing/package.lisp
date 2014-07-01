4491c9b83d637d9d8ac8aaf7c1c949c7ac1212a5
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-07-01 08:24:33 +0000

    export functions

--- image-processing/package.lisp
+++ image-processing/package.lisp
@@ -1,4 +1,22 @@
 (defpackage :image-processing
   (:use :cl)
-  (:export ))
+  (:export
+   #:.linear
+   #:.max
+   #:.min
+   #:.abs
+   #:.abs*
+   #:.log
+   #:.uint8
+   #:.uint16
+   #:next-power-of-two
+   #:extract
+   #:extract-cdf*
+   #:.rr
+   #:.*
+   #:.+
+   #:.phiphi
+   #:.mean
+   #:write-pgm8
+   #:write-pgm16))
 
