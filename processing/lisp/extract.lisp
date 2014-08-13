7c2e7d3955f49859e54e49c1be0b7e70bdcc8a78
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-08-13 08:05:46 +0200

    add a lisp prototype for an extract macro. it still doesnt work as i want it to

---
+++ processing/lisp/extract.lisp
@@ -0,0 +1,131 @@
+(defun .floor (ls value)
+  (mapcar #'(lambda (x) (floor x value)) ls))
+
+(defun .round (ls)
+  (mapcar #'(lambda (x) (round x)) ls))
+
+(defun .- (a b)
+  (if (listp b)
+      (mapcar #'- a b)
+      (mapcar #'(lambda (x) (- x b)) a)))
+
+(defun .+ (a b)
+  (mapcar #'+ a b))
+
+(defun flatten (x)
+  ;; this is from graham's onlisp
+  (labels ((rec (x acc)
+	     (cond ((null x) acc)
+		   ((atom x) (cons x acc))
+		   (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+(defmacro do-region ((&rest rest) &body body)
+  (let ((spec (loop for (name ends starts) in rest collect 
+		   (let ((indices (loop for e in ends collect (gensym))))
+		     (list name (reverse ends) (reverse starts) (reverse indices))))))
+    (labels ((rec (spec acc)
+	       (if (null (second (first spec)))
+		   acc
+		   (rec (loop for (name ends starts indices) in spec collect
+			     (list name (cdr ends) (cdr starts) (cdr indices)))
+			`((loop for
+			       ,@(flatten 
+				  (loop for i from (1- (length spec)) downto 0 and
+					       (name ends starts indices) in spec collect 
+				       (if (= i 0)
+					   `(,(car indices) from ,(car starts) below ,(car ends))
+					   `(,(car indices) from ,(car starts) below ,(car ends) and)))) 
+			     do
+			       ,@acc))))))
+      `(symbol-macrolet (,@(loop for (name ends starts indices) in spec collect
+				`(,name '(aref ,name ,@(loop for ind in indices collect ind)))
+				))
+	 ,(first (rec spec body))))))
+
+#+nil
+(do-region ((src (5 4 2) (0 0 0))
+	     (dst (10 8 4) (0 0 0)))
+  (setf dst src))
+
+
+(defun fun-do-region (rest body)
+  (let ((spec (loop for (name ends starts) in rest collect 
+		   (let ((indices (loop for e in ends collect (gensym))))
+		     (list name (reverse ends) (reverse starts) (reverse indices))))))
+    (labels ((rec (spec acc)
+	       (if (null (second (first spec)))
+		   acc
+		   (rec (loop for (name ends starts indices) in spec collect
+			     (list name (cdr ends) (cdr starts) (cdr indices)))
+			`((loop for
+			       ,@(flatten 
+				  (loop for i from (1- (length spec)) downto 0 and
+					       (name ends starts indices) in spec collect 
+				       (if (= i 0)
+					   `(,(car indices) from ,(car starts) below ,(car ends))
+					   `(,(car indices) from ,(car starts) below ,(car ends) and)))) 
+			     do
+			       ,@acc))))))
+      `(symbol-macrolet () #+nil (,@(loop for (name ends starts indices) in spec collect
+				   `(,name (aref ,name ,@(loop for ind in indices collect ind)))
+				   )) 
+	 ,(first (rec spec body)))
+      )))
+#+nil
+(fun-do-region '((src (5 4 2) (0 0 0))
+		 (dst (10 8 4) (0 0 0)))
+	       '((setf dst src)))
+
+(defun extract (a size &optional
+			 (center (.floor (array-dimensions a) 2))
+			 (border 0))
+  (when (< (length size) (array-rank a))
+    (setf size (append size (subseq (array-dimensions a) (length size)))))
+  (when (< (length center) (array-rank a))
+    (setf center (append center (.floor 
+				 (subseq 
+				  (array-dimensions a) (length size))
+				 2))))
+  (let* ((srccenter (.round center))
+	 (srcstart (.- srccenter (.floor size 2)))
+	 (srcend (.+ srcstart (.- size 1)))
+	 (dststart (loop for e in srcstart collect
+			(if (< e 0)
+			    (- e)
+			    0)))
+	 (dstend (loop for s in size
+		    and se in srcend
+		    and is in (array-dimensions a) collect
+		      (+ s -1
+			 (if (<= is se)
+			     (+ (- se ) is -1)
+			     0))))
+	 (srcend2 (loop for se in srcend
+		     and is in (array-dimensions a) collect
+		       (if (<= is se)
+			   (- is 1)
+			   se)))
+	 (srcstart2 (loop for ss in srcstart collect
+			 (if (< ss 0) 0 ss))))
+    (let ((b (make-array dstend :element-type (array-element-type a)
+			 :initial-element border)))
+      (fun-do-region `((a ,srcend2 ,srcstart2)
+		       (b ,dstend ,dststart))
+		     '((setf b a))))))
+
+#+il
+(let ((a (make-array (list 10))))
+  (extract a '(3)))
+
+#+il
+(let ((a (make-array (list 10))))
+  (eval (extract a '(3))))
+
+#+nil
+(let ((a 3)
+      (q (make-array 4)))
+  (symbol-macrolet ((b (aref q aa)))
+    (let ((aa 2))
+     (setf b a))
+    q))
