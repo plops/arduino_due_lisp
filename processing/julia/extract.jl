7688c9c8cacb82eeeb543d1b656a71cea64a77e6
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-08-19 11:17:04 +0000

    add some debug prints

--- processing/julia/extract.jl
+++ processing/julia/extract.jl
@@ -51,19 +51,24 @@ function extract{T}(a::Array{T}, newsize::Array, center::Array, value::T)
     center = pad_dimensions_from_array(center,a,(x)->div(x,2))
     # convert to int, in case center contains floating point
     srccenter = int(round(center))
+    println("srccenter $srccenter")
     # originating from the center find the coordinates of the first
     # pixel of the source. 
     srcstart = srccenter-div(newsize,2)
+    println("srcstart $srcstart")
     # the result can be <1. in this case the destination needs to be
     # shifted in the positive direction to create an appropriate
     # padding band:
     dststart = [(ss<1)?1-ss:1 for ss in srcstart]
+    println("dststart $dststart")
     # limit the left border of srcstart
     srcstart[srcstart.<1]=1
+    println("srcstart $srcstart")
     # the coordinates of the last pixel of the source. the colon
     # function takes inclusive range as an argument. that explains the
     # -1.    
     srcend   = srcstart+newsize-1
+    println("srcend $srcend")
     # the result can be too big and outside the range of valid
     # coordinates of array a. the largest legal value for srcend is
     # asize(a).  if dstend is within the array bounds srcend<=size(a),
@@ -72,15 +77,17 @@ function extract{T}(a::Array{T}, newsize::Array, center::Array, value::T)
     dstend   = [dststart[i]+newsize[i]-1+
                 ((size(a)[i]<srcend[i])?-srcend[i]+size(a)[i]:0)
                 for i=1:length(srcstart)]
+    println("dstend $dstend")
     # limit the right border of srcend
     outranged = srcend.>asize(a)
     srcend[outranged] = asize(a)[outranged]
+    println("srcend $srcend")
     # create an array of ranges 
     srcrange = map(colon,srcstart,srcend)
     dstrange = map(colon,dststart,dstend)
     out = fill(value,newsize...)
+    println([center srcrange dstrange])
     out[dstrange...] = a[srcrange...]
-    println([srcrange dstrange])
     out
 end
 function extract(a::Array, newsize::Array, center::Array=div(asize(a), 2), value=0)
@@ -97,7 +104,7 @@ function extract(a::Array, newsize::Number, center::Number, value=0)
 end
 
 
-extract([x*10+y for x=1:3,y=1:3],[3,3],[2,2])   
+extract([x*10+y for x=1:3,y=1:3],[3,3],[2,1])   
 
 # example use:
 # [x*10+y for x=1:9,y=1:9]
@@ -109,7 +116,13 @@ using Base.Test
 [0 11  12; 
  0 21  22;
  0 31  32])
- 
+
+
+@test(extract([x*10+y for x=1:3,y=1:3],[3,3],[1,1])==
+[0  0   0;
+ 0 11  12; 
+ 0 21  22])
+
 @test(extract([x*10+y for x=1:3,y=1:3],[3,3])==
 [11  12  13;
  21  22  23;
