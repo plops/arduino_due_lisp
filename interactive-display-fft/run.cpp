89a505b45222e0109a5d5ac4b66bebcdc97fb20d
Martin Kielhorn <kielhorn.martin@gmail.com>
2015-01-08 13:38:07 +0000

    the cameras seem to be opened but the library doesn't reload itself properly

--- interactive-display-fft/run.cpp
+++ interactive-display-fft/run.cpp
@@ -5,8 +5,17 @@
 #include <pylon/PylonIncludes.h>
 #include "radon.h"
 
+using namespace Pylon;
+using namespace GenApi;
+using namespace std;
+
+#define d(cmd)  do{ try{ cmd }			\
+  catch (GenICam::GenericException& e) {        \
+printf( "Exception caught in %s msg=%s",__func__, e.what());	\
+}} while(0)
+
 struct run_state{
-    rfbScreenInfoPtr server;
+  rfbScreenInfoPtr server;
 };
 
 const  int w=512,h=512;
@@ -21,7 +30,28 @@ struct run_state * r_init()
   state->server->frameBuffer=(char*)malloc(w*h*4);
   state->server->alwaysShared=(1==1);
   rfbInitServer(state->server);
+  
+  d(PylonInitialize(););
+  CTlFactory& tlFactory = CTlFactory::GetInstance();
 
+  // Get all attached devices and exit application if no device is found.
+  DeviceInfoList_t devices;
+  d(
+  if ( tlFactory.EnumerateDevices(devices) == 0 ) {
+    printf("no cameras: %ld\n",devices.size());
+  });
+    
+  CInstantCameraArray cameras( min( devices.size(), (long unsigned int) 3));
+  d(
+    // Create and attach all Pylon Devices.
+  for ( size_t i = 0; i < cameras.GetSize(); ++i){
+    cameras[ i ].Attach( tlFactory.CreateDevice( devices[ i ]));
+    // Print the model name of the camera.
+    cout  << "FullName " << cameras[ i ].GetDeviceInfo().GetFullName()
+	  << " serial " << cameras[ i ].GetDeviceInfo().GetSerialNumber() << endl;
+  }
+    );
+  d(cameras.StartGrabbing(););
   return state;
 }
 void r_finalize(struct run_state *state)
@@ -30,6 +60,9 @@ void r_finalize(struct run_state *state)
   rfbShutdownServer(state->server,TRUE);
   free(state->server->frameBuffer);
   rfbScreenCleanup(state->server);
+
+  d(PylonTerminate(););
+
   free(state);
 }
 static int count = 0;
