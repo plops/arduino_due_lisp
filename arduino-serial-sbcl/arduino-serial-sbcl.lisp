5908b45f58bf19fa54385064960e4927bc6b7c97
Martin Kielhorn <kielhorn.martin@gmail.com>
2015-06-12 08:49:35 +0200

    add code to reset arduino using dtr line

--- arduino-serial-sbcl/arduino-serial-sbcl.lisp
+++ arduino-serial-sbcl/arduino-serial-sbcl.lisp
@@ -10,15 +10,15 @@
 	   #:talk-arduino))
 
 (in-package :arduino-serial-sbcl)
- 
+
 (defconstant FIONREAD #x541B)
 (defconstant IXANY #o4000)
 (defconstant CRTSCTS #o20000000000)
- 
+
 (deftype fd-type ()
   `(unsigned-byte 31))
 
-(defun open-serial (tty &key (element-type 'character))
+(defun open-serial (tty &key (element-type 'character) (rate B115200))
   (declare (type (or pathname string) tty)
 	   (values stream fd-type &optional))
   (let* ((fd (sb-posix:open
@@ -27,64 +27,64 @@
 			  O-NDELAY #+nil (we don't wait until dcd is space)
 			  )))
 	 (term (tcgetattr fd))
-	 (baud-rate B115200))
- 
+	 (baud-rate rate))
+
     (fcntl fd F-SETFL (logior O-RDWR O-NOCTTY)) #+nil (reset file status flags, clearing e.g. O-NDELAY)
- 
+
     (cfsetispeed baud-rate term)
     (cfsetospeed baud-rate term)
- 
+
     (macrolet ((set-flag (flag &key (on ()) (off ()))
 		 `(setf ,flag (logior ,@on (logand ,flag ,@off)))))
- 
+
     (setf
      (aref (termios-cc term) VMIN) 1 #+nil (wake up after 32 chars are read)
      (aref (termios-cc term) VTIME) 5 #+nil (wake up when no char arrived for .1 s))
- 
+
      ;; check and strip parity, handshake off
      (set-flag (termios-iflag term)
 	       :on ()
 	       :off (IXON IXOFF IXANY
 		     IGNBRK BRKINT PARMRK ISTRIP
 		     INLCR IGNCR ICRNL))
- 
+
      ;; process output
      (set-flag (termios-oflag term)
 	       :off (OPOST))
- 
+
      ;; canonical input but no echo
      (set-flag (termios-lflag term)
 	       :on ()
 	       :off (ICANON ECHO ECHONL IEXTEN ISIG))
- 
+
      ;; enable receiver, local mode, 8N1 (no parity)
      (set-flag (termios-cflag term)
-	       :on (CLOCAL CREAD 
+	       :on (CLOCAL CREAD
 			   CS8 CRTSCTS)
 	       :off (CSTOPB CSIZE PARENB)))
- 
+
     (tcflush fd TCIFLUSH) #+nil (throw away any input data)
- 
+
     (tcsetattr fd TCSANOW term) #+nil (set terminal port attributes)
     (values
-     (sb-sys:make-fd-stream fd :input t :output t :element-type element-type 
+     (sb-sys:make-fd-stream fd :input t :output t :element-type element-type
 			    :buffering :full)
      fd)))
- 
+
 (defun close-serial (fd)
   (declare (fd-type fd)
 	   (values null &optional))
   (fcntl fd F-SETFL 0) #+nil (reset file status flags, clearing e.g. O-NONBLOCK)
   (sb-posix:close fd) #+nil (this will set DTR low)
   nil)
- 
+
 (defun serial-recv-length (fd)
   (declare (fd-type fd)
 	   (values (signed-byte 32) &optional))
   (sb-alien:with-alien ((bytes sb-alien:int))
     (ioctl fd FIONREAD (sb-alien:addr bytes))
     bytes))
- 
+
 (defun read-response (tty-fd tty-stream)
   (declare (fd-type tty-fd)
 	   (stream tty-stream)
@@ -100,6 +100,21 @@
 	    (setf (char ret i) (read-char tty-stream)))
 	  ret))))
 
+;; the arduino can be restarted by resetting dtr
+;; c code is in the end of tty_ioctl(4)
+;; more information on resetting arduino:
+;; http://playground.arduino.cc/Main/DisablingAutoResetOnSerialConnection
+
+(defun set-dtr (fd val)
+  (declare (fd-type fd)
+           (boolean val)
+           (values (signed-byte 32) &optional))
+  (let ((TIOCM-DTR 2)
+        (TIOCMSET #x5418))
+    (sb-alien:with-alien ((serial sb-alien:int))
+      (setf (ldb (byte 1 TIOCM-DTR) serial) (if val 1 0))
+      (arduino-serial-sbcl::ioctl fd TIOCMSET (sb-alien:addr serial))
+      serial)))
 
 
 (defun write-arduino (tty-stream command)
@@ -247,7 +262,7 @@
  (define (mod x y) (- x (* (/ x y) y)))
  (define (abs x)   (if (< x 0) (- x) x))
  (define (truncate x) x)
- (setq K prog1) 
+ (setq K prog1)
  (define (funcall f . args) (apply f args))
  (define (symbol-function sym) (eval sym))
  (define (symbol-value    sym) (eval sym))
@@ -418,7 +433,7 @@
 
  ;; (defun set-symbol-plist (sym lst)
  ;;  (let ((p (assoc sym *plists*)))
- ;;    (if (null p) 
+ ;;    (if (null p)
  ;;        (setq *plists* (cons (cons sym lst) *plists*))
  ;; 	(rplacd p lst))))
 
@@ -431,10 +446,10 @@
 
  ;; (defun put (sym prop val)
  ;;  (let ((p (assoc sym *plists*)))
- ;;    (if (null p)  
+ ;;    (if (null p)
  ;;        (setq *plists* (cons (list sym prop val) *plists*))
  ;; 	(let ((pr (member prop p)))
- ;; 	  (if (null pr) 
+ ;; 	  (if (null pr)
  ;; 	      (rplacd p (cons prop (cons val (cdr p))))
  ;; 	      (rplaca (cdr pr) val)))))
  ;;  val)
@@ -551,7 +566,7 @@
 		      (read-from-string *femto-lisp-system* nil nil :start start))))))
     (dolist (s sys)
       (format t "~a~%"
-	      (list 
+	      (list
 	       s
 	       (talk-arduino tty-fd tty-stream
 			     (format nil "~a" s)
